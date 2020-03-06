a196732dc942970cfc4cdaa7e9b6c253aebe1722
AnaVomvylas <anavomv@gmail.com>
2020-03-06 20:45:06 +0200

    Javascript done

--- Metronome/wwwroot/js/metronome.js
+++ Metronome/wwwroot/js/metronome.js
@@ -1,1 +1,28 @@
-﻿
+﻿//our main synth
+const synth = Tone.MembraneSynth().toMaster();
+
+const notesSequence = ["F2", "C2", "C2", "C2"];
+
+//play the note in the notesSequence using synth, every beat (4th)
+const sequence = new Tone.Sequence(
+    function (time, note) {
+        synth.triggerAttackRelease(note, "10hz", time);
+    },
+    notesSequence,"4n"
+)
+
+//start it so it's ready to play when Transport starts
+sequence.start();
+
+function playMetronome() {
+    Tone.Transport.stop();
+    Tone.Transport.start();
+}
+
+function stopMetronome() {
+    Tone.Transport.stop();
+}
+
+function TempoOnChange(bpm) {
+    Tone.Transport.bpm = bpm;
+}
